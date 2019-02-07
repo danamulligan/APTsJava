802b045542c8afe372130195f29d54c50576eb3c
Dana Mulligan <danamulligan@The-Matrix-2019.local>
2019-02-06 22:53:13 -0500

    finished Anonymous

--- src/Anonymous.java
+++ src/Anonymous.java
@@ -1,9 +1,55 @@
 public class Anonymous {
-      public int howMany(String[] headlines, String[] messages) 
-      {
-            // fill in code here
-    	    // this uses hashsets!!
-    	  
-            return 0;
-      }
-   }
+	public int howMany(String[] headlines, String[] messages) 
+	{
+		// fill in code here
+
+		int count = 0;
+		
+		int[] countsH = countH(headlines);
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+		for(int k=0; k<messages.length; k++) 
+		{
+			boolean valid = true;
+			int[] countsM = countM(messages[k]);
+			for(char ch : alphabet.toCharArray())
+			{
+				if(countsH[ch]<countsM[ch])
+				{
+					valid = false;
+				}
+			}
+			if(valid)
+				count++;
+		}
+		return count;
+	}
+	public int[] countH(String[] headlines)
+	{
+		int[] countsH = new int[256];
+		for(String s : headlines)
+		{
+			for (char ch : s.toLowerCase().toCharArray())
+			{
+				if (ch != ' ')
+				{
+					countsH[ch] = countsH[ch] + 1;
+				}
+			}
+		}
+		return countsH;
+	}
+	public int[] countM(String s)
+	{
+		int[] countsM = new int[256];
+		for (char ch : s.toLowerCase().toCharArray())
+		{
+			if (ch != ' ')
+			{
+				countsM[ch] = countsM[ch] + 1;
+			}
+		}
+
+		return countsM;
+	}
+}
--- src/Encryption.java
+++ src/Encryption.java
@@ -1,4 +1,54 @@
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Encryption {
+	public String encrypt(String message){
+		// you write code here
+		int counter = 'a';
+		String[] letters = message.split("");
+		String ret = "";
+
+
+
+
+		return ret;
+	}
+	public int place(String[] letters)
+	{
+		HashSet<String> known = new HashSet<>();
+		char[] seen = new char[numUnique(letters)];
+
+		for(int k=0; k<seen.length; k++)
+		{
+			for (int r=0; r<letters.length; r++)
+			{
+				if (letters[r].equals(seen[k]))
+				{
+					return k;
+				}
+				if (!known.contains(letters[r]]))
+				{
+					
+				}
+				known.add(letters[r]);
+				
+			}
+		}
+
+		return k;
+	}
+	public int numUnique(String[] letters) {
+		HashSet<String> let = new HashSet<>();
+		int unique = 0;
+		for(int k=0; k<letters.length; k++)
+		{
+			if(!let.contains(letters[k]))
+			{
+				unique += 1;
+			}
+			let.add(letters[k]);
+		}
+		return unique;
+	}
 
 }
