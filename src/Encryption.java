3bc888edd68f80f60703530addff3747038ea6cf
Dana Mulligan <danamulligan@The-Matrix-2019.local>
2019-02-06 23:13:33 -0500

    Finished Encryption

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,54 +1,24 @@
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class Encryption {
 	public String encrypt(String message){
-		// you write code here
-		int counter = 'a';
-		String[] letters = message.split("");
-		String ret = "";
 
-
-
-
-		return ret;
-	}
-	public int place(String[] letters)
-	{
-		HashSet<String> known = new HashSet<>();
-		char[] seen = new char[numUnique(letters)];
-
-		for(int k=0; k<seen.length; k++)
-		{
-			for (int r=0; r<letters.length; r++)
-			{
-				if (letters[r].equals(seen[k]))
-				{
-					return k;
-				}
-				if (!known.contains(letters[r]]))
-				{
-					
-				}
-				known.add(letters[r]);
-				
-			}
+		char counter = 'a';
+		char[] cyph = new char[256];
+		String encoded = "";
+		
+		for (int k=0; k<256; k++) {
+			cyph[k] = ' ';
 		}
 
-		return k;
-	}
-	public int numUnique(String[] letters) {
-		HashSet<String> let = new HashSet<>();
-		int unique = 0;
-		for(int k=0; k<letters.length; k++)
-		{
-			if(!let.contains(letters[k]))
+		for (char ch : message.toCharArray()) {
+			if(cyph[ch] == ' ') 
 			{
-				unique += 1;
+				cyph[ch] = counter;
+				counter++;
 			}
-			let.add(letters[k]);
+			encoded += cyph[ch];
 		}
-		return unique;
+		return encoded;
 	}
-
 }
